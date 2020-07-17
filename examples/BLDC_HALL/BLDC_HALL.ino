3d6b9102dc28fcf343965b7749f35b09d5634909
BrixInMotion <kilian.brixel2@infineon.com>
2020-07-17 12:37:23 +0200

    Update BLDC hall picture

--- examples/BLDC_HALL/BLDC_HALL.ino
+++ examples/BLDC_HALL/BLDC_HALL.ino
@@ -1,11 +1,11 @@
 /**
  * This example code is for BLDC motor control with Hall Sensor.
- * Please be noticed it has to be modified for diffenrent motor (e.g. the motor with differnet pole number, PI-values, ...)
+ * Please be noticed it has to be modified for a diffenrent motor (e.g. motor with differnet pole number, PI-values, ...)
  * Refer to the README.md in this folder.
 */
 
 #include "IFX007T-Motor-Control.h"
-uint8_t rpmSpeed = 3000;
+uint8_t rpmSpeed = 3000;      // initial speed
 bool direction = 0;           // 0 or 1
 
 //Create an instance of 'IFX007TMotorControl' called 'MyMotor'
@@ -18,7 +18,7 @@ void setup()
 
   MyMotor.begin();
   // Adapt the following values according to the README if necessary
-  MyMotor.MotorParam.MotorPoles = 12;       // Count the coils in your motor (only necessary for accurate RPM speed) 
+  MyMotor.MotorParam.MotorPoles = 12;       // Pole pair number
   MyMotor.MotorParam.SensingMode = 1;       // If you use a Hallsensor set 1, for sensorless application 0
   
   MyMotor.configureBLDCMotor(MyMotor.MotorParam);
@@ -31,7 +31,7 @@ void loop()
   if (Serial.available() > 0)
   {
     uint8_t in = Serial.read();
-    if(in == 'u') rpmSpeed += 100;
+    if(in == 'u') rpmSpeed += 100;          // Adapt the speed with keyboard input in the serial monitor
     if(in == 'j') rpmSpeed -= 100;
   }
   
